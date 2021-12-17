9037a898d48b6a4309d799156c621d072dbabf74
Mathias Buus <mathiasbuus@gmail.com>
2021-12-17 14:44:16 +0100

    first commit - missing cjs source maps and error handling

---
+++ README.md
@@ -0,0 +1,75 @@
+# script-linker
+
+CJS/MJS source loader that can preresolve imports/requires so linking them on runtime runs much faster.
+
+Features include:
+
+* Simple transforms (ie all contained on the same line)
+* Source Maps for all transforms so debugging code is easy
+* IO agnostic, bring your own IO
+* Similar CJS/ESM interop like Node.js
+* Cross platform (wip)
+
+## Usage
+
+``` js
+const ScriptLinker = require('script-linker')
+
+const s = new ScriptLinker({
+  builtins: {
+    has (path) {
+      // return true if builtin, false otherwise
+    },
+    get (path) {
+      // return the builtin module for that path
+    }
+  },
+  map (path, { isImport, isBuiltin, isSourceMap }) {
+    // return a url that is actually passed to import
+    // a default method is provided
+  },
+  readFile (name) {
+    return fs.promises...
+  },
+  stat (name) { // optional call to be used if you cache module resolutions somewhere
+    return {
+      type: module.type,
+      resolutions: module.resolutions,
+      cache: integerIndicatingACacheIdForName
+    }
+  }
+})
+
+const module = await s.load('/some/module.js')
+
+console.log(module.type) // module/json/commonjs
+console.log(module.resolutions) // the resolved modules
+console.log(module.source) // original source
+console.log(module.toESM()) // transform to esm with imports preresolved
+console.log(module.toCJS()) // transform to cjs with requires preresolved
+console.log(module.geneateSourceMap()) // generate a source map
+```
+
+In the process executing the module, you need to include the ScriptLinker runtime dep.
+Currently that's done by running
+
+```js
+// Run this in the render/module process.
+// Sets up a global object, global[Symbol.for('scriptlinker')], that is used to make modules run.
+// Has no nodejs/native deps so can be browserified if preferred.
+
+ScriptLinker.preload({
+  builtins, // same as above
+  map, // same as above
+  getSync (url) {
+    // resolve this url synchronously (ie xhr sync or equi)
+    // the url is ALWAYS a url returned from map so you should know
+    // how to resolve the url and return the relevant content, ie cjs/esm/sourcemap string
+  },
+  resolveSync (req, dirname, { isImport }) {
+    // resolve the import/require request ie "./foo.js" or "fs"
+    // from the directory passed. isImport indicates if this is a require or import() call.
+    // should return the absolute path to the module.
+  }
+})
+```
---
+++ index.js
@@ -0,0 +1,281 @@
+const Mod = require('./lib/module')
+const defaultCompile = require('./lib/compile')
+const resolveModule = require('resolve')
+const isProperty = require('is-property')
+const b4a = require('b4a')
+const path = require('path')
+const Module = require('module')
+
+const defaultBuiltins = {
+  has (ns) {
+    return Module.builtinModules.includes(ns)
+  },
+  get (ns) {
+    return require(ns)
+  }
+}
+
+class ScriptLinker {
+  constructor ({ map = defaultMap, builtins = defaultBuiltins, stat, readFile, isFile, isDirectory }) {
+    this.map = map
+    this.modules = new Map()
+    this.builtins = builtins
+
+    this._userStat = stat || null
+    this._userReadFile = readFile || null
+    this._userIsFile = isFile || null
+    this._userIsDirectory = isDirectory || null
+  }
+
+  _isFile (name, cb) {
+    if (this._userIsFile) {
+      this._userIsFile(name).then((yes) => cb(null, yes), cb)
+    } else {
+      this._userReadFile(name).then(() => cb(null, true), () => cb(null, false))
+    }
+  }
+
+  _isDirectory (name, cb) {
+    if (this._userIsDirectory) {
+      this._userIsDirectory(name).then((yes) => cb(null, yes), cb)
+    } else {
+      this._userReadFile(name).then(() => cb(null, false), () => cb(null, true))
+    }
+  }
+
+  _readFile (name, cb) {
+    this._userReadFile(name).then((buf) => cb(null, buf), cb)
+  }
+
+  async findPackageJSON (filename, { directory = false } = {}) {
+    let dirname = directory ? filename : path.dirname(filename)
+
+    while (true) {
+      try {
+        const src = await this._userReadFile(path.join(dirname, 'package.json'))
+        return JSON.parse(typeof src === 'string' ? src : b4a.from(src))
+      } catch {
+        const next = path.join(dirname, '..')
+        if (next === dirname) return null
+        dirname = next
+      }
+    }
+  }
+
+  async load (filename) {
+    let m = this.modules.get(filename)
+
+    if (m) {
+      await m.refresh()
+      return m
+    }
+
+    m = new Mod(this, filename)
+    this.modules.set(filename, m)
+
+    try {
+      await m.refresh()
+      return m
+    } catch (err) {
+      this.modules.delete(filename)
+      throw err
+    }
+  }
+
+  async resolve (ns, basedir, { isImport = true } = {}) {
+    const self = this
+
+    return new Promise((resolve, reject) => {
+      resolveModule(ns, {
+        basedir,
+        extensions: ['.js', '.mjs', '.cjs'],
+        realpath (name, cb) {
+          cb(null, name)
+        },
+        isFile: (name, cb) => {
+          self._isFile(name, cb)
+        },
+        isDirectory: (name, cb) => {
+          self._isDirectory(name, cb)
+        },
+        readFile: (name, cb) => {
+          self._readFile(name, cb)
+        },
+        packageFilter (pkg, pkgfile, dir) {
+          if (isImport) {
+            const esmMain = getPath(pkg, ['exports', 'import']) || getPath(pkg, ['exports', '.', 'import'])
+            if (esmMain) pkg.main = esmMain
+          }
+          return pkg
+        },
+        pathFilter (pkg, path, relativePath) {
+          // console.log('path:', path, relativePath, pkg)
+          return relativePath
+        }
+      }, function (err, res) {
+        if (err) return reject(err)
+        resolve(res)
+      })
+    })
+  }
+
+  static preload (opts) {
+    const {
+      map = defaultMap,
+      builtins = defaultBuiltins,
+      compile = defaultCompile,
+      getSync,
+      resolveSync
+    } = opts
+
+    const SLModule = defineModule()
+
+    const sl = global[Symbol.for('scriptlinker')] = {
+      Module: SLModule,
+      require: null,
+      createImport (filename, doImport) {
+        const dirname = path.dirname(filename)
+        return (req) => {
+          try {
+            const r = resolveImport(dirname, req)
+            return doImport(r)
+          } catch {
+            return Promise.reject(new Error(`Cannot find package '${req}' imported from ${filename}`))
+          }
+        }
+      },
+      createRequire (filename, parent = null) {
+        if (!parent) {
+          parent = new SLModule(filename, null)
+          parent.filename = filename
+        }
+
+        require.resolve = resolve
+        require.cache = SLModule._cache
+        require.extensions = SLModule._extensions
+        require.main = undefined
+
+        return require
+
+        function resolve (request, opts) {
+          return SLModule._resolveFilename(request, parent, false, opts)
+        }
+
+        function require (request, opts) {
+          return parent.require(request, opts)
+        }
+      }
+    }
+
+    sl.require = sl.createRequire('/')
+
+    return sl
+
+    function resolveImport (dirname, req) {
+      const isBuiltin = builtins.has(req)
+      return map(isBuiltin ? req : resolveSync(req, dirname, { isImport: true }), { isImport: true, isBuiltin, isSourceMap: false })
+    }
+
+    function getExtension (filename) {
+      const i = filename.lastIndexOf('.')
+      return i > -1 ? filename.slice(i) : filename
+    }
+
+    function defineModule () {
+      function Module (id = '', parent) {
+        this.id = id
+        this.path = path.dirname(id) // TODO: always use "posix" paths
+        this.exports = {}
+        this.filename = null
+        this.loaded = false
+        this.children = [] // TODO: not updated atm
+        this.paths = []
+        this.parent = parent
+      }
+
+      Module._cache = Object.create(null)
+      Module._pathCache = Object.create(null) // only there in case someone inspects it
+      Module.Module = Module
+      Module.syncBuiltinESMExports = () => {} // noop for now
+      Module.globalPaths = []
+      Module.builtinModules = [] // not populated atm but could be if we need to ...
+
+      Module.createRequire = function (filename) {
+        return sl.createRequire(filename, null)
+      }
+
+      Module._resolveFilename = function (request, parent, isMain, opts, dirname) {
+        if (opts && opts.resolved) return request
+        if (request === 'module') return request // special case for this module
+        return builtins.has(request) ? request : resolveSync(request, parent ? parent.path : dirname, { isImport: false })
+      }
+
+      Module._load = function (request, parent, isMain, opts) {
+        const filename = Module._resolveFilename(request, parent, isMain, opts)
+        const cached = Module._cache[filename]
+        if (cached) return cached.exports
+        if (filename === 'module') return Module
+        if (builtins.has(filename)) return builtins.get(filename)
+        const mod = new Module(filename, parent)
+        Module._cache[filename] = mod
+        let threw = true
+        try {
+          mod.load(filename, opts)
+          threw = false
+        } finally {
+          if (threw) {
+            delete Module._cache[filename]
+          }
+        }
+        return mod.exports
+      }
+
+      Module._extensions = {
+        '.js': function (mod, filename, opts) {
+          mod._compile(Module._getSource(filename, opts), filename)
+        },
+        '.json': function (mod, filename, opts) {
+          console.log('in .json')
+          mod.exports = JSON.parse(Module._getSource(filename, opts))
+        }
+      }
+
+      Module._getSource = function (filename, opts) {
+        return typeof (opts && opts.source) === 'string' ? opts.source : getSync(map(filename, { isImport: false, isBuiltin: false, isSourceMap: false }))
+      }
+
+      Module.prototype._compile = function (source, filename) {
+        compile(this, this.exports, this.path, filename, sl.createRequire(this.path, this), source)
+      }
+
+      Module.prototype.require = function (request, opts) {
+        return Module._load(request, this, false, opts)
+      }
+
+      Module.prototype.load = function (filename, opts) {
+        this.filename = filename
+        const ext = Module._extensions[getExtension(filename)] || Module._extensions['.js']
+        ext(this, filename, opts)
+        this.loaded = true
+      }
+
+      return Module
+    }
+  }
+}
+
+ScriptLinker.defaultCompile = defaultCompile
+ScriptLinker.defaultMap = defaultMap
+ScriptLinker.defaultBuiltins = defaultBuiltins
+ScriptLinker.defaultUserspace = '-'
+
+module.exports = ScriptLinker
+
+function getPath (o, path) {
+  for (let i = 0; i < path.length && o; i++) o = o[path[i]]
+  return o
+}
+
+function defaultMap (id, { isImport, isBuiltin, isSourceMap }) {
+  return (isImport ? 'module://' : 'commonjs://' ) + (isBuiltin ? '' : ScriptLinker.defaultUserspace) + id + (isSourceMap ? '.map' : '')
+}
