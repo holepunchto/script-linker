e353bb7f8b58cab7bff65145fc936eeb28b8f7ac
utanapishtim <>
2022-01-12 07:26:53 -0800

    fix lint errors

--- test/plain.test.js
+++ test/plain.test.js
@@ -9,12 +9,12 @@ test('it should load plain vanilla js modules', async ({ is }) => {
   const mod = await linker.load(path.resolve(__dirname, '../node_modules/jquery/dist/jquery.js'))
   const esm = await mod.toESM()
   const runtime = ScriptLinker.runtime({
-    resolveSync() { return `data:text/javascript,${encodeURIComponent(esm)}` },
+    resolveSync () { return `data:text/javascript,${encodeURIComponent(esm)}` },
     map (x) { return x }
   })
   const myImport = runtime.createImport(path.resolve(__dirname, '../node_modules/jquery'), (s) => import(s))
   const { default: $ } = await myImport('jquery')
-  const dom = new JSDOM(`<!DOCTYPE html><p id="message">It worked!</p>`)
+  const dom = new JSDOM('<!DOCTYPE html><p id="message">It worked!</p>')
   const els = $(dom.window)('#message')
   is(els.length, 1)
 })
