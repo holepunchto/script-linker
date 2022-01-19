8244eaf8741ec818e9e587701e12eaf03e8ab4d6
Mathias Buus <mathiasbuus@gmail.com>
2022-01-19 21:29:05 +0100

    made a simple bundler that works without runtime deps

--- index.js
+++ index.js
@@ -159,6 +159,71 @@ class ScriptLinker {
     })
   }
 
+  async bundle (filename) {
+    if (!this.bare) throw new Error('Bundling only works in bare mode')
+
+    const cjs = []
+
+    let esm = ''
+    let main = null
+    let out = '{\n'
+
+    for await (const mod of this.topologicWalk(filename)) {
+      if (main === null) main = mod
+      if (esm) esm += ',\n'
+      esm += '    ' + JSON.stringify('bundle:' + mod.filename) + ': '
+      esm += JSON.stringify('data:application/javascript;base64,' + Buffer.from(mod.toESM()).toString('base64'))
+      if (mod.type !== 'module') cjs.push(mod)
+    }
+    if (esm) esm += '\n'
+
+    if (esm) {
+      out += '{\n  const im = document.createElement(\'script\')\n'
+      out += '  im.type = \'importmap\'\n'
+      out += '  im.textContent = JSON.stringify({ imports: {\n'
+      out += esm
+      out += '  }})\n'
+      out += '  document.currentScript.before(im)\n}\n'
+    }
+
+    if (cjs.length) {
+      out += 'require.cache = {\n'
+      for (let i = cjs.length - 1; i >= 0; i--) {
+        out += '  ' + JSON.stringify(cjs[i].filename) + ': { exports: {} }' + (i === 0 ? '' : ',') + '\n'
+      }
+      out += '}\n'
+      out += 'require.scope = function (map, fn) {\n'
+      out += '  scopedRequire.cache = require.cache\n'
+      out += '  fn(scopedRequire)\n'
+      out += '  function scopedRequire (req) {\n'
+      out += '    if (!map.hasOwnProperty(req)) throw new Error("Cannot require \'" + req + "\'")\n'
+      out += '    return require(map[req])\n'
+      out += '  }\n'
+      out += '}\n'
+      for (let i = cjs.length - 1; i >= 0; i--) {
+        const mod = cjs[i]
+        const map = mod.requireMap() || {}
+        out += 'require.scope(' + JSON.stringify(map) + ', function (require, '
+        out += '__filename = ' + JSON.stringify(mod.filename) + ', '
+        out += '__dirname = ' + JSON.stringify(mod.dirname) + ', '
+        out += 'module = require.cache[' + JSON.stringify(mod.filename) + ']) {\n'
+        out += mod.toCJS()
+        out += '})\n'
+      }
+      out += 'function require (req) {\n'
+      out += '  if (!require.cache.hasOwnProperty(req)) throw new Error("Cannot require \'" + req + "\'")\n'
+      out += '  return require.cache[req].exports\n'
+      out += '}\n'
+    }
+
+    if (main.type === 'module') {
+      out += 'import(' + JSON.stringify('bundle:' + main.filename) + ')\n'
+    }
+
+    out += '}\n'
+    return out
+  }
+
   static runtime (opts) {
     return runtime(opts)
   }
