2e1f14643b28b1a87c097884890f25d0a263ea19
Mathias Buus <mathiasbuus@gmail.com>
2022-09-14 18:23:53 +0200

    test fix for latest node

--- test/experimental-loader.test.mjs
+++ test/experimental-loader.test.mjs
@@ -1,22 +1,22 @@
-import unixresolve from 'unix-path-resolve'
+import path from 'path'
 import url from 'url'
 import { spawn } from 'child_process'
 import { once } from 'events'
 import test from 'brittle'
 
-const __dirname = unixresolve(url.fileURLToPath(import.meta.url), '..')
+const __dirname = path.join(url.fileURLToPath(import.meta.url), '..')
 
 test('it should intercept resolve and load calls for esm', async ({ is, teardown }) => {
   const child = spawn(
     process.execPath,
     [
       '--experimental-loader',
-      unixresolve(__dirname, './fixtures/experimental-loader/loader.mjs'),
-      unixresolve(__dirname, './fixtures/experimental-loader/main.js')
+      new URL('file://' + path.join(__dirname, './fixtures/experimental-loader/loader.mjs')).href,
+      path.join(__dirname, './fixtures/experimental-loader/main.js')
     ],
     { stdio: 'pipe' }
   )
-
+  child.stderr.pipe(process.stderr)
   teardown(() => child.kill('SIGINT'))
   const buf = await once(child.stdout, 'data')
   is(buf.toString().trim(), 'an export')
--- test/fixtures/experimental-loader/loader.mjs
+++ test/fixtures/experimental-loader/loader.mjs
@@ -4,18 +4,22 @@ export async function resolve (specifier, context, defaultResolve) {
   try {
     if (module.builtinModules.includes(specifier)) return defaultResolve(specifier, context)
     const { _rpc } = global[Symbol.for('scriptlinker')]
-    return _rpc.request('resolveESM', { specifier, context })
+    const reply = { ...(await _rpc.request('resolveESM', { specifier, context })), shortCircuit: true }
+    if (!reply.url.includes(':')) reply.url = new URL('file://' + reply.url).href
+    return reply
   } catch {
     return defaultResolve(specifier, context)
   }
 }
 
 export async function load (url, context, defaultLoad) {
+  if (url.startsWith('file:')) url = new URL(url).href
   try {
     if (/^node:/.test(url)) return defaultLoad(url, context, defaultLoad)
     const { _rpc } = global[Symbol.for('scriptlinker')]
-    return _rpc.request('getESM', { url, context })
-  } catch {
+    const reply = { ...(await _rpc.request('getESM', { url, context })), shortCircuit: true }
+    return reply
+  } catch  {
     return defaultLoad(url, context, defaultLoad)
   }
 }
